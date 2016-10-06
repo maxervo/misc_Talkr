2da6a2436da89298b99a62e79a1b0bc2bfe55932
Guillaume <Yuu44@users.noreply.github.com>
2016-10-06 15:22:19 +0200

    DONE !

--- src/jalon01/client.c
+++ src/jalon01/client.c
@@ -1,43 +1,87 @@
-#include <stdlib.h>
-#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include <netdb.h>
+#include <netinet/in.h> // struct sockaddr_in
+
 #include <string.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
+#define h_addr h_addr_list[0]   /* for backward compatibility */
+#define BUFFER_SIZE 256
+#define QUIT_MSG "\\quit"
+
+int main(int argc, char const *argv[]) {
+  int sockfd;
+  int port_no;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+  char buffer[BUFFER_SIZE];
+
+  if (argc < 3) {
+    fprintf(stderr,"Program %s needs arguments regarding target server: hostname, port\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  port_no = atoi(argv[2]);
+
+  //Open socket
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // IPV4 TCP TCP
+  if (sockfd <0) {
+    perror("Error: socket opening");
+    exit(EXIT_FAILURE);
+  }
+
+  //Get server
+  server = gethostbyname(argv[1]);	//maybe later use addrinfo
+  if (server == NULL) {
+    fprintf(stderr, "Error: No such host\n");
+    exit(EXIT_FAILURE);
+  }
+
+  //Init server struct
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  memcpy(server->h_addr, &serv_addr.sin_addr.s_addr, server->h_length);  //why can't do assign direct? because of network order endian?
+  serv_addr.sin_port = htons(port_no);  //convert to network order
+
+  //Connect to server
+  if ( connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))<0 ) {
+    perror("Error: connection");
+    exit(EXIT_FAILURE);
+  }
+  printf("-------------------------\n- Welcome to the chat ! -\n-------------------------\n");
+  printf("write '\\\\quit' in order to close this session \n\n");
+  printf("Input msg:\n ");
+
+  while  (1){
+    printf("\n >>  ");
+    memset(buffer, 0, BUFFER_SIZE);
+    fgets(buffer, 20, stdin);
+
+    //Write
+    int sent=0;
+    do{
+      sent+= write(sockfd, buffer+sent, 20-sent);		//maybe encompass it in while security loop, or use send...etc
+    } while (sent!=20);
 
 
+    printf("\nMsg sent: %s", buffer);
+    if (strcmp(buffer, QUIT_MSG)<=0)
+      exit(EXIT_SUCCESS);
 
-int main(int argc,char** argv)
-{
+    //Read echo
+    memset(buffer, 0, BUFFER_SIZE);
+    int readen=0;
+    do{
+      readen+= read(sockfd, buffer+readen, 20-readen);		//maybe encompass it in while security loop, or use send...etc
+    } while (readen!=20);
+    buffer[readen+1] = '\0'; // vu sur developpez.com
 
-
-    if (argc != 3)
-    {
-        fprintf(stderr,"usage: RE216_CLIENT hostname port\n");
-        return 1;
-    }
-
-//get address info from the server
-//get_addr_info()
-
-
-//get the socket
-//s = do_socket()
-
-//connect to remote socket
-//do_connect()
+    printf("\nEcho received: %s", buffer);
+  }
 
 
-//get user input
-//readline()
-
-//send message to the server
-//handle_client_message()
-
-
-    return 0;
-
-
+  return EXIT_SUCCESS;
 }
--- src/jalon01/server.c
+++ src/jalon01/server.c
@@ -9,15 +9,17 @@
 #define MAX_CONC_CONNECTIONS 20
 #define MAX_NUM_QUEUE 10
 #define BUFFER_SIZE 256
+#define QUIT_MSG "\\quit"
 
 void error(const char *msg);
-void handle(int sockfd);
+int handle(int sockfd);
 
 int main(int argc, char* argv[]) {
   struct sockaddr_in serv_addr, cli_addr;
   int sockfd, new_sockfd;
   int cli_len = sizeof(cli_addr);
   int port_no;
+
 
   if (argc != 2)
   {
@@ -35,7 +37,7 @@ int main(int argc, char* argv[]) {
   //Init socket struct
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
-  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);  //convert to network order
   serv_addr.sin_port = htons(port_no);  //convert to network order
 
   //Bind
@@ -47,17 +49,21 @@ int main(int argc, char* argv[]) {
   listen(sockfd, MAX_NUM_QUEUE);
 
   //Accept Queue
-  for (int i=0; i < MAX_CONC_CONNECTIONS; i++)
-  {
-    new_sockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
-    if (new_sockfd < 0) {
-      error("Error: accept");
+  while(1){
+    int i;
+    for (i=0; i < MAX_CONC_CONNECTIONS; i++)
+    {
+      new_sockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_len);
+      if (new_sockfd < 0) {
+        error("Error: accept");
+      }
+      while(handle(new_sockfd)){          // while the connection is open
+      //handle(new_sockfd);
+      }
+      close(new_sockfd);
     }
-    handle(new_sockfd);
-    close(new_sockfd);
   }
   close(sockfd);
-
   return EXIT_SUCCESS;
 }
 
@@ -67,26 +73,35 @@ void error(const char *msg)   //ATTENTION : program flow exit
     exit(EXIT_FAILURE);
 }
 
-void handle(int sockfd) {
+int handle(int sockfd) {
   char buffer[BUFFER_SIZE];
   memset(buffer, 0, BUFFER_SIZE);
 
   //Read
-  if (read(sockfd, buffer, 32) < 0) {
-    error("Error: read");
-  }
+  int readen=0;
+  do{
+    readen+= read(sockfd, buffer+readen, 20-readen);		//maybe encompass it in while security loop, or use send...etc
+  } while (readen!=20);
 
-  if (strcmp(buffer, "/quit") == 0) {
+  char quit[6]; // \\quit
+  strncpy(quit,buffer,6);
+  buffer[readen+1] = '\0'; // vu sur developpez.com
+
+  if (strcmp(quit,QUIT_MSG) <= 0) {
     printf("Connection closed by the client\n");
     close(sockfd);
+    return 0;
   }
   else {
-    printf("msg received : %s\n", buffer);
+    printf("msg received : %s", buffer);
   }
 
   //Echo
-  if (write(sockfd, buffer, 32) < 0) {
-    error("Error: write echo");
-  }
-  printf("Echo sent\n");
+  int sent=0;
+  do{
+    sent+= write(sockfd, buffer+sent, 20-sent);		//maybe encompass it in while security loop, or use send...etc
+  } while (sent!=20);
+
+  printf("Echo sent\n----------------------------\n----------------------------\n");
+  return 1;
 }
